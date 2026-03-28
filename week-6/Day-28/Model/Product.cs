2975ea406591224a39c88bc6375044b8f10b5a95
Durgasony12 <125243630+Durgasony12@users.noreply.github.com>
2026-03-28 07:27:36 +0530

    Add files via upload

---
+++ week-6/Day-28/Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int PID { get; set; }
+        public string Pname { get; set; }
+        public decimal Price { get; set; }
+        public string Brand { get; set; }
+        //public int Deptno { get; set; }
+    }
+}
