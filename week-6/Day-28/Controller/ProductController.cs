682ac3934cec48fbed4be5c979d03f6235291d82
Durgasony12 <125243630+Durgasony12@users.noreply.github.com>
2026-03-28 07:26:58 +0530

    Add files via upload

---
+++ week-6/Day-28/Controller/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> product = new List<Product>
+            {
+                new Product {PID = 101, Pname="Mobile", Price= 45000,Brand="Samsung" },
+                new Product {PID = 102, Pname="Shirt", Price= 900,Brand="H&M"},
+                new Product {PID = 103, Pname="shoes", Price= 1500,Brand="converse"},
+                new Product {PID = 104, Pname="Face Wash", Price= 400,Brand="Simple"},
+            };
+
+            return View(product);
+        }
+        public IActionResult Details()
+        {
+            Product ProObj = new Product()
+            { PID= 101,Pname="Mobile",Price=50000,Brand="Apple" };
+
+            return View(ProObj);
+        }
+    }
+}
