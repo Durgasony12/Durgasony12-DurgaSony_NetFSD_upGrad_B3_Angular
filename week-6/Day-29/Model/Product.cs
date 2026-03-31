90d3ae8d329694547db2a31e783e01ee14a9c17d
Durgasony12 <125243630+Durgasony12@users.noreply.github.com>
2026-03-31 06:49:06 +0530

    Add files via upload

---
+++ week-6/Day-29/Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        [Required]
+        public int PID { get; set; }
+        [Required]
+        [StringLength(15, MinimumLength = 5)]
+        public string Pname { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [StringLength(15, MinimumLength = 5)]
+        public string Brand { get; set; }
+        
+    }
+}
