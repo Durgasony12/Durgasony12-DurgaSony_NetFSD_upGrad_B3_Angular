87e117414bb49dcb31669bfb5c58f27cdc7c6063
Durgasony12 <125243630+Durgasony12@users.noreply.github.com>
2026-04-02 08:09:19 +0530

    Add files via upload

---
+++ week-7/Day-32/Di-problem/Repositories/ContactRepository.cs
@@ -0,0 +1,28 @@
+﻿using WebApplication2.Models;
+using Microsoft.EntityFrameworkCore;
+namespace WebApplication2.Repositories
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContactRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Contacts> GetAll()
+        {
+            return _context.Contacts.ToList();
+        }
+
+        public Contacts GetById(int id)
+        {
+            return _context.Contacts.Find(id);
+        }
+        public void Add(Contacts Contact)
+        {
+            _context.Contacts.Add(Contact);
+            _context.SaveChanges();
+        }
+    }
+}
