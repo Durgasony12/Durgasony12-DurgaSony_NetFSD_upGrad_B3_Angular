6bbcfbdee63a658e4733a7e5d915ef3ddc4732f9
Durgasony12 <125243630+Durgasony12@users.noreply.github.com>
2026-04-02 08:08:51 +0530

    Add files via upload

---
+++ week-7/Day-32/Di-problem/Models/Contacts.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WebApplication2.Models
+{
+    public class Contacts
+    {
+        
+        public int ContactId { get; set; }  // Primary Key
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CompanyName { get; set; }
+        public string EmailId { get; set; }
+        public long MobileNo { get; set; }
+        public string Designation { get; set; } 
+    }
+}
