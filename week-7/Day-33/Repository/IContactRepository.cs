fab61ade7ef645e320b96b0456cdf3a16256b551
Durgasony12 <125243630+Durgasony12@users.noreply.github.com>
2026-04-02 17:26:25 +0530

    Add files via upload

---
+++ week-7/Day-33/Repository/IContactRepository.cs
@@ -0,0 +1,11 @@
+﻿using WebApplication2.Models;
+namespace WebApplication2.Repositories
+{
+    public interface IContactRepository
+    {
+        IEnumerable<Contacts> GetAll();
+        Contacts GetById(int id);
+        void Add(Contacts contact);
+         
+    }
+}
