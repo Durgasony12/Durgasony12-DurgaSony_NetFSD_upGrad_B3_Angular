d68f1395c32fb0d7b2ec6a218f057c8fd45e0f6a
Durgasony12 <125243630+Durgasony12@users.noreply.github.com>
2026-04-02 08:56:54 +0530

    Add files via upload

---
+++ week-7/Day-32/Di-problem/services/ContactService.cs
@@ -0,0 +1,28 @@
+﻿using WebApplication2.Models;
+using WebApplication2.Repositories;
+namespace WebApplication2.Services
+{
+    public class ContactService : IContactService
+    {
+        private readonly IContactRepository _repository;
+
+        public ContactService(IContactRepository repository)
+        {
+            _repository = repository;
+        }
+        public IEnumerable<Contacts> GetAllContacts()
+        {
+            return _repository.GetAll();
+        }
+
+        public Contacts GetContactById(int id)
+        {
+            return _repository.GetById(id);
+        }
+        public void Create(Contacts contact)
+        {
+            _repository.Add(contact);
+        }
+
+    }
+}
