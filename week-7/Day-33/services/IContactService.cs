150e37cbf98907fac58e149c3e9f1303a997e241
Durgasony12 <125243630+Durgasony12@users.noreply.github.com>
2026-04-02 17:27:02 +0530

    Add files via upload

---
+++ week-7/Day-33/services/IContactService.cs
@@ -0,0 +1,11 @@
+﻿using WebApplication2.Models;
+namespace WebApplication2.Services
+{
+    public interface IContactService
+    {
+        IEnumerable<Contacts> GetAllContacts();
+        Contacts GetContactById(int id);
+        //void AddContact(Contacts contact);
+        void Create(Contacts Contact);
+    }
+}
