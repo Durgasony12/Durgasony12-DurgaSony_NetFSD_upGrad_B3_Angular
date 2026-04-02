b7e734fa832a5bde95c9f1cb292e0375315ec566
Durgasony12 <125243630+Durgasony12@users.noreply.github.com>
2026-04-02 09:01:30 +0530

    Add files via upload

---
+++ week-7/Day-32/MovieCatalog/Repository/IMovieRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieEntity = Movie.Models.Movie;
+namespace Movie.Repositories
+{
+    public interface IMovieRepository
+    {
+        IEnumerable<MovieEntity> GetAll();
+        MovieEntity GetById(int id);
+        void Add(MovieEntity Movie);
+        void Update(MovieEntity Movie);
+        void Delete(int id);
+    }
+}
---
+++ week-7/Day-32/MovieCatalog/Repository/MovieRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieEntity = Movie.Models.Movie;
+namespace Movie.Repositories
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MovieRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<MovieEntity> GetAll()
+        {
+            return _context.Movies.ToList();
+        }
+        public MovieEntity GetById(int id)
+        {
+            return _context.Movies.Find(id);
+        }
+        public void Add(MovieEntity Movie)
+        {
+            _context.Movies.Add(Movie);
+            _context.SaveChanges();
+        }
+        public void Update(MovieEntity Movie)
+        {
+            _context.Movies.Update(Movie);
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            var movie = _context.Movies.Find(id);
+
+            if (movie != null)
+            {
+                _context.Movies.Remove(movie);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
