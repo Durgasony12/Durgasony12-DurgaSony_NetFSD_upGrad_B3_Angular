4fc5098f1c14fa56098792f6b390f4bc5dbdc616
Durgasony12 <125243630+Durgasony12@users.noreply.github.com>
2026-04-02 09:02:03 +0530

    Add files via upload

---
+++ week-7/Day-32/MovieCatalog/services/IMovieService.cs
@@ -0,0 +1,12 @@
+﻿using MovieEntity = Movie.Models.Movie;
+namespace Movie.Services
+{
+    public interface IMovieService
+    {
+        IEnumerable<MovieEntity> GetMovies();
+        MovieEntity GetMovieById(int id);
+        void CreateMovie(MovieEntity Movie);
+        void UpdateMovie(MovieEntity Movie);
+        void DeleteMovie(int id);
+    }
+}
---
+++ week-7/Day-32/MovieCatalog/services/MovieService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieEntity = Movie.Models.Movie;
+using Movie.Repositories;
+namespace Movie.Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _repository;
+
+        public MovieService(IMovieRepository repository)
+        {
+            _repository = repository;
+        }
+        public IEnumerable<MovieEntity> GetMovies()
+        {
+            return _repository.GetAll();
+        }
+        public MovieEntity GetMovieById(int id)
+        {
+            return _repository.GetById(id);
+        }
+        public void CreateMovie(MovieEntity movie)
+        {
+            _repository.Add(movie);
+        }
+
+        public void UpdateMovie(MovieEntity movie)
+        {
+            _repository.Update(movie);
+        }
+
+        public void DeleteMovie(int id)
+        {
+            _repository.Delete(id);
+        }
+
+
+    }
+}
