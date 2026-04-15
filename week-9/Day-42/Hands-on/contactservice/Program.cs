70774285ce86b48a245cd30da22200b717dc64e9
Durgasony12 <125243630+Durgasony12@users.noreply.github.com>
2026-04-16 00:18:21 +0530

    Add files via upload

---
+++ week-9/Day-42/Hands-on/contactservice/Program.cs
@@ -0,0 +1,42 @@
+
+namespace ContactManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddHttpClient();
+
+            //var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
